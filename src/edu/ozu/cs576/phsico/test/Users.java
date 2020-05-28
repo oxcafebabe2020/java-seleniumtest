aa158723a6fc6c6483376b5b5ddf4c9b242448b1
oxcafebabe2020 <65906913+oxcafebabe2020@users.noreply.github.com>
2020-05-28 12:49:38 +0300

    Add files via upload

---
+++ src/edu/ozu/cs576/phsico/test/Users.java
@@ -0,0 +1,18 @@
+package edu.ozu.cs576.phsico.test;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * purpose : bu class 1 kullanicinin yapabilecegi tum islemleri test eder. login
+ * , rezervasyon ekrani goruntuleme, meeting, makale gosterimi, profile ekrani
+ * ve logout
+ */
+public class Users implements ITest {
+
+	@Override
+	public void run(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
